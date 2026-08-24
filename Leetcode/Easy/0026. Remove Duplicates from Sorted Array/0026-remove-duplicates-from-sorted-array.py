ae8986e11b7eca5fecd1debc665d1000f8a23c7c
Varanasi Sakeeth <sakethvaranasi7@gmail.com>
2026-08-24 14:47:56 +0530

    [LeetCode][Easy] 0026-remove-duplicates-from-sorted-array - Time: 3 ms (44.08%), Space: 20.6 MB (42.9%) - AlgorithmHub

---
+++ Leetcode/Easy/0026. Remove Duplicates from Sorted Array/0026-remove-duplicates-from-sorted-array.py
@@ -0,0 +1,12 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+
+        count = 1
+        for i in range(1,len(nums)):
+            if nums[i] != nums[i-1]:
+                nums[count] = nums[i]
+                count += 1
+        return count
+        
--- README.md
+++ README.md
@@ -6,10 +6,10 @@ Archive of accepted coding challenge solutions, synced by [AlgorithmHub](https:/
 
 | Platform | Solved |
 | --- | ---: |
-| LeetCode | 15 |
+| LeetCode | 16 |
 | 프로그래머스 | 0 |
 | HackerRank | 67 |
-| Total | 82 |
+| Total | 83 |
 
 ## Platforms
 
